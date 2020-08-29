cadf51c38be57d2d83f79a84813bc4032bc2fa34
Roooger04 <rogerrogerionascimento01@gmail.com>
2020-08-29 00:00:29 +0000

    28/08

--- arrow-functions.js
+++ arrow-functions.js
@@ -15,3 +15,31 @@ console.log('---------------')
 console.log(aleatorio())
 
 let aleatorio2 = () => Math.floor(Math.random() * 1000)
+
+let imc = function(peso, altura){
+    return peso / (altura ** 2)
+}
+
+let imc2 = (peso, altura) => peso / (altura ** 2)
+
+console.log(imc(87, 1.78))
+console.log(imc2(87, 1.78))
+
+let fatorial = function(n){
+    let res = 1
+    for(let i = n; i > 1; i--){
+        res *= i
+    }
+    return res
+}
+
+let fatorial2 = n => {
+    let res = 1
+    for(let i = n; i > 1; i--){
+        res*= i
+    }
+    return res
+}
+
+console.log(fatorial(5))
+console.log(fatorial2(5))
